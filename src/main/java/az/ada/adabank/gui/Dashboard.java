dae574b99c523525fa58431fef19f0757bb82330
Adil Aliyev <adil@neats.az>
2015-01-09 18:27:01 +0400

    Person window added.

--- src/main/java/az/ada/adabank/gui/Dashboard.java
+++ src/main/java/az/ada/adabank/gui/Dashboard.java
@@ -18,8 +18,8 @@ import javax.swing.WindowConstants;
  * @author Adil Aliyev
  */
 public class Dashboard extends JFrame {
-    
-    private JButton jButton1;
+
+    private JButton personButton;
     private JButton jButton2;
     private JButton jButton3;
     private JButton jButton4;
@@ -29,7 +29,7 @@ public class Dashboard extends JFrame {
     private JScrollPane jScrollPane1;
     private JScrollPane jScrollPane3;
     private JTextArea jTextArea2;
-    
+
     private GroupLayout layout;
     private GroupLayout jPanel1Layout;
 
@@ -39,56 +39,62 @@ public class Dashboard extends JFrame {
     public Dashboard() {
         initComponents();
     }
-    
+
     private void initComponents() {
-        
+
         jPanel1 = new JPanel();
         jScrollPane1 = new JScrollPane();
         jList1 = new JList();
         jComboBox1 = new JComboBox();
-        jButton1 = new JButton();
+        personButton = new JButton();
         jButton2 = new JButton();
         jScrollPane3 = new JScrollPane();
         jTextArea2 = new JTextArea();
         jButton3 = new JButton();
         jButton4 = new JButton();
-        
+
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        
+
         jList1.setModel(new AbstractListModel() {
             String[] strings = {"Item 1", "Item 2", "Item 3", "Item 4", "Item 5"};
-            
+
             public int getSize() {
                 return strings.length;
             }
-            
+
             public Object getElementAt(int i) {
                 return strings[i];
             }
         });
         jScrollPane1.setViewportView(jList1);
-        
+
         jComboBox1.setModel(new DefaultComboBoxModel(new String[]{"Item 1", "Item 2", "Item 3", "Item 4"}));
-        
-        jButton1.setText("New Customer");
-        
+
+        personButton.setText("New Person");
+
         jButton2.setText("New Organization");
-        
+
         jButton3.setText("New Account");
-        
+
         jButton4.setText("New Transaction");
-        
+
         jTextArea2.setColumns(20);
         jTextArea2.setRows(5);
         jScrollPane3.setViewportView(jTextArea2);
-        
+
+        personButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                personButtonActionPerformed(evt);
+            }
+        });
+
         setUpLayout();
-        
+
         pack();
     }
-    
+
     private void setUpLayout() {
-        
+
         jPanel1Layout = new GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -108,17 +114,16 @@ public class Dashboard extends JFrame {
                         .addComponent(jScrollPane1, GroupLayout.DEFAULT_SIZE, 270, Short.MAX_VALUE)
                         .addContainerGap())
         );
-        
+
         layout = new GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-                layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+        layout.setHorizontalGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
                 .addGroup(layout.createSequentialGroup()
                         .addComponent(jPanel1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
                                 .addGroup(layout.createSequentialGroup()
-                                        .addComponent(jButton1)
+                                        .addComponent(personButton)
                                         .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
                                         .addComponent(jButton2)
                                         .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
@@ -130,13 +135,12 @@ public class Dashboard extends JFrame {
                                         .addComponent(jScrollPane3)
                                         .addContainerGap())))
         );
-        layout.setVerticalGroup(
-                layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+        layout.setVerticalGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
                 .addComponent(jPanel1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(layout.createSequentialGroup()
                         .addContainerGap()
                         .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                                .addComponent(jButton1)
+                                .addComponent(personButton)
                                 .addComponent(jButton2)
                                 .addComponent(jButton3)
                                 .addComponent(jButton4)
@@ -146,5 +150,14 @@ public class Dashboard extends JFrame {
                         .addContainerGap())
         );
     }
-    
+
+    private void personButtonActionPerformed(java.awt.event.ActionEvent evt) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Person().setVisible(true);
+            }
+        });
+
+    }
+
 }
