d47677495c6f74bd6228baaa01c9a203f6f07c2d
Emil <Emil@10.133.3.227>
2015-01-10 13:59:06 +0400

    Created import packages for the Account class. Reorganized code into  a more propper coding style.

--- src/main/java/az/ada/adabank/entities/Customer.java
+++ src/main/java/az/ada/adabank/entities/Customer.java
@@ -2,6 +2,14 @@ package az.ada.adabank.entities;
 
 public class Customer {
 
-    public String ID;
+    private String id;
+    public void setId(String id)
+    {
+        this.id=id;
+    }
+    public String getId()
+    {
+        return id;
+    }
 
 }
